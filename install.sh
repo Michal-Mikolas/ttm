e7f400eed12e89880cfa9bdf0d4c9f54106acb5f
Michal Mikolas <nanuqcz@gmail.com>
2021-02-06 17:00:01 +0100

    Added telepot into install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-pip install ccxt
-pip install keyring
-pip install dateutil
-pip install yagmail
-pip install tabulate
-pip install urllib3==1.24.1  # hotfix for telepot: https://github.com/nickoala/telepot/issues/463
+pip3 install ccxt
+pip3 install keyring
+pip3 install dateutil
+pip3 install yagmail
+pip3 install tabulate
+pip3 install telepot
+pip3 install urllib3==1.24.1  # hotfix for telepot: https://github.com/nickoala/telepot/issues/463
